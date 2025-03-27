df312e10219bd5b3dea61ccd73fd9ff3663eb1d3
Nazmus Sakib <nazmuss247@gmail.com>
2025-03-27 23:43:02 +0600

    Uploading if..else

---
+++ Python-learning/00_Python Basic/17.0_If_Else.py
@@ -0,0 +1,29 @@
+#If statement
+a = 30
+b = 60
+if b > a :
+    print("B is greater than a")
+
+#Taking user input and comparing by if statement
+"""
+a = input("Input first Number:")
+b = input("Input second Number:")
+if a > b :
+    print("A is greater than B")
+else:
+    print("B is greater than A")
+"""
+
+#elif
+#The elif keyword is Python's way of saying "if the previous conditions were not true, then try this condition".
+a = 100
+b = 100
+if a > b :
+    print("A is greater than B")
+elif a == b :
+    print("Both are equal")
+else:
+    print("B is greater")
+
+#else statement
+#The else keyword catches anything which isn't caught by the preceding conditions

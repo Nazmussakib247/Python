5cd305ea0f2e385303f203e1f1bcc3b0327d87b4
Nazmus Sakib <nazmuss247@gmail.com>
2025-04-08 20:21:11 +0600

    Uploading matplotlib

---
+++ Python-learning/09_Matplotlib/41_Matplotlib_Basic.py
@@ -0,0 +1,11 @@
+#Installation of Matplotlib
+"""
+pip install matplotlib
+"""
+
+#Import Matplotlib
+import matplotlib
+
+#Checking Matplotlib Version
+import matplotlib
+print(matplotlib.__version__)
